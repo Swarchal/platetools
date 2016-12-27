8c0202b72f2bd72239b7c0cabe338323a1bff63a
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-12-27 20:52:26 +0000

    tabs to spaces

--- R/plate_map.R
+++ R/plate_map.R
@@ -15,19 +15,19 @@ plate_map <- function(data, well){
 
     # if not a 1536 well plate, we can just convert well labels to numbers
     if (!is_1536(well)){
-	platemap <- mutate(platemap,
-		Row = as.numeric(match(toupper(substr(well, 1,1)), LETTERS)),
-		Column = as.numeric(substr(well, 2, 5)))
+    platemap <- mutate(platemap,
+    Row = as.numeric(match(toupper(substr(well, 1,1)), LETTERS)),
+    Column = as.numeric(substr(well, 2, 5)))
     } else {
-	# if a 1536 plate cannot convert the well labels directly to row and column
-	# values as we have double well ID's, i.e 'AA' isn't a number
-	Row <- as.numeric(match(toupper(substr(well, 1, 1)), LETTERS))
-	add_to_row <- ifelse(nchar(as.character(well)) == 4, 26, 0)
-	Row <- Row + add_to_row
+    # if a 1536 plate cannot convert the well labels directly to row and column
+    # values as we have double well ID's, i.e 'AA' isn't a number
+    Row <- as.numeric(match(toupper(substr(well, 1, 1)), LETTERS))
+    add_to_row <- ifelse(nchar(as.character(well)) == 4, 26, 0)
+    Row <- Row + add_to_row
 
-	platemap <- mutate(platemap,
-	    Row = Row,
-	    Column = as.numeric(substr(well, nchar(as.character(well)) - 1, 5)))
+    platemap <- mutate(platemap,
+        Row = Row,
+        Column = as.numeric(substr(well, nchar(as.character(well)) - 1, 5)))
 
     }
     platemap['values'] <- data
@@ -50,9 +50,9 @@ plate_map <- function(data, well){
 #' @export
 #'
 plate_map_scale <- function(data, well){
-	df <- plate_map(data, well)
-	df$values <- scale(df$values)
-	return(df)
+    df <- plate_map(data, well)
+    df$values <- scale(df$values)
+    return(df)
 }
 
 #' creates dataframe of row, column, plate_id from data regarding wellIDs
@@ -66,9 +66,9 @@ plate_map_scale <- function(data, well){
 #' @export
 
 plate_map_grid <- function(data, well, plate_id){
-	df <- plate_map(data, well)
-	df$plate_label <- plate_id
-	return(df)
+    df <- plate_map(data, well)
+    df$plate_label <- plate_id
+    return(df)
 }
 
 #' creates dataframe of row, column, plate_id from data regarding wellIDs
@@ -85,17 +85,17 @@ plate_map_grid <- function(data, well, plate_id){
 #' @export
 
 plate_map_grid_scale <- function(data, well, plate_id, each){
-	df <- plate_map_grid(data, well, plate_id)
+    df <- plate_map_grid(data, well, plate_id)
 
-	if (each == FALSE){
-		df$values <- scale(df$values)
-	} else if (each == TRUE){
-		df <- df %>% group_by_("plate_label") %>%
-			mutate_(values = "scale(values)[,]") %>% #STOP SCALE RETURNING STUPID ATTRIBUTES!
-			ungroup() %>%
-			as.data.frame()
-	}
-	return(df)
+    if (each == FALSE){
+    df$values <- scale(df$values)
+    } else if (each == TRUE){
+    df <- df %>% group_by_("plate_label") %>%
+    mutate_(values = "scale(values)[,]") %>% #STOP SCALE RETURNING STUPID ATTRIBUTES!
+    ungroup() %>%
+    as.data.frame()
+    }
+    return(df)
 }
 
 
@@ -112,8 +112,8 @@ plate_map_grid_scale <- function(data, well, plate_id, each){
 plate_map_multiple <- function(data, well){
     platemap <- as.data.frame(well)
     platemap <- mutate(platemap,
-	       Row = as.numeric(match(toupper(substr(well, 1, 1)), LETTERS)),
-	          Column = as.numeric(substr(well, 2, 5)))
+           Row = as.numeric(match(toupper(substr(well, 1, 1)), LETTERS)),
+              Column = as.numeric(substr(well, 2, 5)))
     platemap_out <- data.frame(platemap, data)
     return(platemap_out)
 }
