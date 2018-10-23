557176f9a8aee55453d09ca8baaf351edd812a7c
Scott Warchal <s.warchal@sms.ed.ac.uk>
2018-10-23 17:32:05 +0100

    move column labels to the top of the plate

--- R/plot_wrapper.R
+++ R/plot_wrapper.R
@@ -15,7 +15,7 @@ plt96 <- function(platemap){
         geom_point(aes_string(fill = "values"), colour = "gray20", shape = 21, size = 10) +
         coord_fixed(ratio = (13 / 12) / (9 / 8), xlim = c(0.5, 12.5), ylim = c(0.5, 8.5)) +
         scale_y_reverse(breaks = seq(1, 8), labels = LETTERS[1:8]) +
-        scale_x_continuous(breaks = seq(1, 12)) +
+        scale_x_continuous(position = "top", breaks = seq(1, 12)) +
         xlab("") +
         ylab("")
 }
@@ -37,7 +37,7 @@ plt384 <- function(platemap){
         geom_point(aes_string(fill = "values"), colour = "gray20", shape = 22, size = 5) +
         coord_fixed(ratio = (24.5 / 24) / (16.5 / 16), xlim = c(0.5, 24.5), ylim = c(0.5, 16.5)) +
         scale_y_reverse(breaks = seq(1, 16), labels = LETTERS[1:16]) +
-        scale_x_continuous(breaks = seq(1, 24)) +
+        scale_x_continuous(position = "top", breaks = seq(1, 24)) +
         xlab("") +
         ylab("")
 }
@@ -59,7 +59,7 @@ plt1536 <- function(platemap){
     geom_point(aes_string(fill = "values"), colour = "gray20", shape = 22, size = 3.5) +
     coord_fixed(ratio = (48.25 / 48) / (32.25 / 32), xlim = c(0.5, 48.5), ylim = c(0.5, 32.5)) +
     scale_y_reverse(breaks = seq(1, 32)) +
-    scale_x_continuous(breaks = seq(1, 48)) +
+    scale_x_continuous(position = "top", breaks = seq(1, 48)) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.25)) +
     xlab("") +
     ylab("")
