58fafdddb65838718066389de7a2e79d0035dcdf
Scott Warchal <s.warchal@sms.ed.ac.uk>
2018-12-05 12:08:19 +0000

    more size and shape parameters (towards #14)

--- R/raw_map.R
+++ R/raw_map.R
@@ -5,6 +5,7 @@
 #' @param data Numerical values to be plotted
 #' @param well Vector of well identifiers e.g "A01"
 #' @param plate Number of wells in complete plate (96, 384 or 1536)
+#' @param ... additional parameters to plot wrappers
 #'
 #' @import ggplot2
 #'
@@ -20,8 +21,7 @@
 #'         well = df$well,
 #'         plate = 384)
 
-raw_map <- function(data, well,
-                    plate = 96) {
+raw_map <- function(data, well, plate = 96, ...) {
 
     check_plate_input(well, plate)
 
@@ -29,14 +29,14 @@ raw_map <- function(data, well,
     platemap <- plate_map(data, well)
 
     if (plate == 96){
-    plt <- plt96(platemap) +
-        theme_bw()
+    plt <- plt96(platemap, ...) + theme_bw()
+
     } else if (plate == 384){
-    plt <- plt384(platemap) +
-        theme_bw()
+    plt <- plt384(platemap, ...) + theme_bw()
+
     } else if (plate == 1536L){
-    plt <- plt1536(platemap) +
-        theme_bw()
+    plt <- plt1536(platemap, ...) + theme_bw()
+
     } else{
     stop("Invalid argument for 'plate'. \nOption: 96, 384 or 1546",
         call. = FALSE)
