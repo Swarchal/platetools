76b37e3010e1dc0e8c3a88690ec5b2f074e6974c
Scott Warchal <s.warchal@sms.ed.ac.uk>
2018-12-11 11:53:31 +0000

    un-dplyr read_map() (towards #16)

--- R/read_map.R
+++ R/read_map.R
@@ -17,32 +17,28 @@
 #' @return dataframe with new column named after `new_col_name`
 
 
-read_map <- function (data, map,
-                      verbose = TRUE,
-                      new_col_name = "header"){
+read_map <- function (data, map, verbose = TRUE, new_col_name = "header") {
 
     if (is.character(map) == TRUE) {
-    map <- as.matrix(read.csv(map, header = FALSE))
+        map <- as.matrix(read.csv(map, header = FALSE))
     }
-    map <- as.matrix(map)
-    platemap <- mutate(data,
-    row = as.numeric(match(toupper(substr(data$well,1, 1)), LETTERS)),
-        column = as.numeric(substr(data$well, 2, 5)))
 
+    map <- as.matrix(map)
+    platemap <- data
+    platemap$row <- as.numeric(match(toupper(substr(data$well,1, 1)), LETTERS))
+    platemap$column <- as.numeric(substr(data$well, 2, 5))
     platemap$header <- NULL
 
     for (i in 1:nrow(data)) {
-    platemap$header[i] <- as.vector(with(platemap,
-                                         map[[row[i],column[i]]]))
+        platemap$header[i] <- as.vector(with(platemap, map[[row[i],column[i]]]))
     }
 
     names(platemap)[ncol(platemap)] <- new_col_name
 
     if (verbose == FALSE) {
-    platemap$row <- NULL
-    platemap$column <- NULL
+        platemap$row <- NULL
+        platemap$column <- NULL
+    }
+
     return(platemap)
-    } else if (verbose == TRUE) {
-    return(platemap)
-    } else stop("Argument 'verbose' requires a boolean.", call. = FALSE)
 }
