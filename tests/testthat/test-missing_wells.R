9c691c1c48d791d95f0209a35ab2ab8e79934367
Swarchal <scott.warchal@gmail.com>
2019-04-04 09:26:30 +0200

    fix error in missing_wells tests (fixes #19)

--- tests/testthat/test-missing_wells.R
+++ tests/testthat/test-missing_wells.R
@@ -1,26 +1,24 @@
 context("missing_wells")
 
 # create data with no missing wells
-full <- data.frame(val = rnorm(96),
-		   well = num_to_well(1:96))
+full <- data.frame(val = rnorm(96), well = num_to_well(1:96))
 
 # create data with missing wells
 # missing last 6 wells
-missing <- data.frame(val = rnorm(90),
-		      well = num_to_well(1:90))
+missing <- data.frame(val = rnorm(90), well = num_to_well(1:90))
 
 out_full <- missing_wells(full, well = 'well')
 out_missing <- missing_wells(missing, well = 'well')
 
 
 test_that("missing_wells errors when expected",{
-     expect_error(missing_wells(full, well = "well", plate = 1))
-	 expect_error(missing_wells(full, well = TRUE))
-     expect_error(missing_wells(full, well = full$well))
-	 expect_error(missing_wells(as.list(full), well = full$well),
-	 expect_error(missing_wells(full, well = missing$wel)))
-	 expect_error(missing_wells(as.matrix(full), well = full$well))
-	 expect_error(missing_wells(fill, well = full$not_valid_col))
+    expect_error(missing_wells(full, well = "well", plate = 1))
+    expect_error(missing_wells(full, well = TRUE))
+    expect_error(missing_wells(full, well = full$well))
+    expect_error(missing_wells(as.list(full), well = full$well))
+    expect_error(missing_wells(full, well = missing$well))
+    expect_error(missing_wells(as.matrix(full), well = full$well))
+    expect_error(missing_wells(fill, well = full$not_valid_col))
 })
 
 test_that("missing wells doesn't return anything for a full plate",{
