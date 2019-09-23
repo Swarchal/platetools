9b065f7dc6e136acea3f743bc8d3112dd390ca36
Aaron McKenna <aaronmck@uw.edu>
2019-09-22 23:31:37 -0400

    format it right

--- R/check_plate_input.R
+++ R/check_plate_input.R
@@ -5,11 +5,11 @@
 
 check_plate_input <- function(well, plate) {
     if (length(well) > plate){
-        warning(paste("Warning: Invalid plate selection. The well data given (",length(well),") has more entries then number of wells in the selected plate (",plate,").\nAre you sure argument 'plate' is correct for the number of wells in your data?\nnote: Default is a 96-well plate.",
+        warning(paste("Warning: Invalid plate selection. The well data given (",length(well),") has more entries then number of wells in the selected plate (",plate,").\nAre you sure argument 'plate' is correct for the number of wells in your data?\nnote: Default is a 96-well plate."),
                 call. = FALSE)
     }
     if (plate > 2 * length(well)){
-        warning("Warning: Your well label count (",length(well),") covers less than half the selected plate(",plate,").\nAre you sure argument 'plate' is correct for the number of wells in your data?\nnote: Default is a 96-well plate.",
+        warning(paste("Warning: Your well label count (",length(well),") covers less than half the selected plate(",plate,").\nAre you sure argument 'plate' is correct for the number of wells in your data?\nnote: Default is a 96-well plate."),
                 call. = FALSE)
     }
 }
