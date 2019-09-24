76618987db4e37acc246ac1bcb3eae082c304fe7
Aaron McKenna <aaronmck@uw.edu>
2019-09-24 08:53:16 -0400

    Simple warning message change (#21)
    * more informative warnings

--- R/check_plate_input.R
+++ R/check_plate_input.R
@@ -5,11 +5,11 @@
 
 check_plate_input <- function(well, plate) {
     if (length(well) > plate){
-        warning("Invalid plate selection. The data given has more rows then number of wells.\nAre you sure argument 'plate' is correct for the number of wells in your data?\nnote: Default is a 96-well plate.",
+        warning(paste("Warning: Invalid plate selection. The well data given (",length(well),") has more entries then number of wells in the selected plate (",plate,").\nAre you sure argument 'plate' is correct for the number of wells in your data?\nnote: Default is a 96-well plate."),
                 call. = FALSE)
     }
     if (plate > 2 * length(well)){
-        warning("Invalid plate selection. The data given has more rows then number of wells.\nAre you sure argument 'plate' is correct for the number of wells in your data?\nnote: Default is a 96-well plate.",
+        warning(paste("Warning: Your well label count (",length(well),") covers less than half the selected plate(",plate,").\nAre you sure argument 'plate' is correct for the number of wells in your data?\nnote: Default is a 96-well plate."),
                 call. = FALSE)
     }
 }
