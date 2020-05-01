0ed107b2517a2b3554d5b5cf34bfd65b9dc86b81
Scott Warchal <scott.warchal@gmail.com>
2020-05-01 16:34:22 +0200

    fix for coord_fixed ylim change (towards #22)

--- R/plot_wrapper.R
+++ R/plot_wrapper.R
@@ -16,7 +16,7 @@ plt96 <- function(platemap, size = 10, shape = 21){
                    aes_string(x = "Var1", y = "Var2"),
                    color = "grey90", fill = "white", shape = shape, size = size-4, alpha = 0.1) +
         geom_point(aes_string(fill = "values"), colour = "gray20", shape = shape, size = size) +
-        coord_fixed(ratio = (13 / 12) / (9 / 8), xlim = c(0.5, 12.5), ylim = c(0.5, 8.5)) +
+        coord_fixed(ratio = (13 / 12) / (9 / 8), xlim = c(0.5, 12.5), ylim = c(8.5, 0.5)) +
         scale_y_reverse(breaks = seq(1, 8), labels = LETTERS[1:8]) +
         scale_x_continuous(position = "top", breaks = seq(1, 12)) +
         xlab("") +
@@ -41,7 +41,7 @@ plt384 <- function(platemap, size = 5, shape = 22){
                    aes_string(x = "Var1", y = "Var2"),
                    color = "grey90", fill = "white", shape = shape, size = size-2, alpha = 0.1) +
         geom_point(aes_string(fill = "values"), colour = "gray20", shape = shape, size = size) +
-        coord_fixed(ratio = (24.5 / 24) / (16.5 / 16), xlim = c(0.5, 24.5), ylim = c(0.5, 16.5)) +
+        coord_fixed(ratio = (24.5 / 24) / (16.5 / 16), xlim = c(0.5, 24.5), ylim = c(16.5, 0.5)) +
         scale_y_reverse(breaks = seq(1, 16), labels = LETTERS[1:16]) +
         scale_x_continuous(position = "top", breaks = seq(1, 24)) +
         xlab("") +
@@ -67,7 +67,7 @@ plt1536 <- function(platemap, size = 3.5, shape = 22){
                aes_string(x = "Var1", y = "Var2"),
                color = "grey90", fill = "white", shape = shape, size = size-1.5, alpha = 0.1) +
     geom_point(aes_string(fill = "values"), colour = "gray20", shape = shape, size = size) +
-    coord_fixed(ratio = (48.25 / 48) / (32.25 / 32), xlim = c(0.5, 48.5), ylim = c(0.5, 32.5)) +
+    coord_fixed(ratio = (48.25 / 48) / (32.25 / 32), xlim = c(0.5, 48.5), ylim = c(32.5, 0.5)) +
     scale_y_reverse(breaks = seq(1, 32)) +
     scale_x_continuous(position = "top", breaks = seq(1, 48)) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.25)) +
